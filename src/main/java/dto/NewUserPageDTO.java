8d835ea7d75ce394af610c66e7e9ae9b286bca20
Lito <Lito@192.168.0.102>
2015-09-15 12:39:14 +0300

    NewUserPageDTO

---
+++ src/main/java/dto/NewUserPageDTO.java
@@ -0,0 +1,74 @@
+package dto;
+
+public class NewUserPageDTO {
+
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	//private int age;
+	private String eMail; 
+	private String userPassword;
+	private String userRole;
+	
+	public NewUserPageDTO() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+/*
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+*/
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+	
+}
